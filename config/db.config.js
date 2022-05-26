b685c7d5ccc7a7276b09821d1f92e5dafe19f9ac
MarcoPQ27 <marco.balderaspq@gmail.com>
2022-05-25 22:09:18 -0500

    credenciales

--- config/db.config.js
+++ config/db.config.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 
 /* Conexion local a la base de datos */
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'node_mysql_crud_db'
+  host     : 'byk9i8mybf4sd74ljwcp-mysql.services.clever-cloud.com',
+  user     : 'ujhuqqzphb5qexzs',
+  password : 'vuktUcUvyF51PZRTmvlh',
+  database : 'byk9i8mybf4sd74ljwcp'
 })
 
 dbConn.connect(err => {
