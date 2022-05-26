fe4161036a2e37566e03e532bca9f487cfd43914
Marco_Dev <marcob@infoexpo.com.mx>
2022-05-26 10:24:51 -0500

    update package.json

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const app = express()
 
 /* Configuración del puerto del servidor */
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.use(cors())
 
 /* analizar solicitudes de tipo de contenido - application/x-www-form-urlencoded */
